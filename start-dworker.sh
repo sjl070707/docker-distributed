a83e15c8c73839be0ffcbae4beb3352d36d83f6a
jin <sjl070707@gmail.com>
2017-03-05 14:03:41 -0500

    dask 1.16

--- start-dworker.sh
+++ start-dworker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 NCPUS=`python -c "import multiprocessing as mp; print(mp.cpu_count())"`
 echo "Detected $NCPUS cpus"
-pip install dask[complete] s3fs
+pip install s3fs
 # Alternatively start one python process per CPU
 # exec su $BASICUSER -c "env PATH=$PATH dask-worker dscheduler:8786 --nthreads 1 --nprocs $NCPUS $*"
 exec su $BASICUSER -c "env PATH=$PATH dask-worker dscheduler:8786 $*"
